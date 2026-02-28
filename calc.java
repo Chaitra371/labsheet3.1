ef40bd73d766a1a82903c8921e008140c167a5ec
Chaitra371 <chaitra0920@gmail.com>
2026-02-28 15:08:16 +0530

    Add basic calculator functionality in calc.java

---
+++ calc.java
@@ -0,0 +1,22 @@
+package cdv58;
+
+public class Calculator {
+		
+
+		public static void main(String[] args) {
+			int a=10;
+			int b=20;
+			char op = '+';
+			if( op == '+') {
+				System.out.println( a+b);
+			}
+			else {
+				System.out.println("0");
+			}
+
+			
+			
+
+		}
+
+	}
